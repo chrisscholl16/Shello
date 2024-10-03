67e80f65131a3c4d8e5b25c86933805b649d51c2
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-10-02 20:05:24 -0400

    Vid 2&3 User & Error pages

--- app.py
+++ app.py
@@ -7,11 +7,23 @@ app = Flask(__name__)
 # creating routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    first_name = 'Chris'
+    stuff = "This is <strong>Bold</strong> text"
+
+    favorite_pizza = ['Pepperoni', 'Chreese', 'Sausage', 41]
+    return render_template('index.html', first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
 
 @app.route('/user/<name>')
 def user(name):
-    return "<h1>Hello {}!!!</h1>".format(name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 # running the server with debug by hitting play 
 if __name__ == '__main__':
